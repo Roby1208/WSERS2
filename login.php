0ee37716b6e18e9fb6a71c5976f92370239692b6
roby <triro299@school.lu>
2020-04-20 16:03:18 +0200

    update2

--- login.php
+++ login.php
@@ -1,54 +1,71 @@
 <?php
 include_once "sessionCheck.php";
+include_once "credentials.php";
 
-if($_SESSION["UserLogged"]){
-    print "you already have logged in". "<br>";
-
+function displayUserDetails($connection)
+{
+    $userFromMyDatabase = $connection->prepare("SELECT * FROM ppl WHERE UserName=?");
 }
-    elseif (isset($_POST["Username"]) && isset($_POST["Password"])) {
-    include_once "credentials.php";
-    // Create connection
-    $connection = mysqli_connect($servername, $username, $password, $database);
-    // Check connection
-    if (!$connection) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    $userFromMyDatabase = $connection->prepare("SELECT * FROM ppl WHERE UserName=?"
-    );
-    $userFromMyDatabase->bind_param("s", $_POST["Username"]);
-    $userFromMyDatabase->execute();
-    $result = $userFromMyDatabase->get_result();
-    if ($result->num_rows === 1) {
-        print "We are checking your password <BR>";
-        $row = $result->fetch_assoc();
+$userFromMyDatabase->bind_param("i", $_SESSION["CurrentUser"]);
+  $userFromMyDatabase->execute();
+  $result = $userFromMyDatabase->get_result();
+  $row = $result->fetch_assoc();
+  print "First name:" . $row ["First_name"] . "<br>";
+  print "Second name:" . $row ["Second_name"] . "<br>";
+  print "Age:" . $row ["Age"] . "<br>";
+  print "Username:" . $row ["Username"] . "<br>";
+  $countrySelect = $connect ->prepare("SELECT COUONTRY_NAME FROM countries WHERE COUNTRY_ID=?");
+  $countrySelect->bind_param("i", $row["Nationality"]);
+  $countrySelect->execute();
+  $resultContry= $countrySelect->get_result();
+  $rowCountry = $resultContry->fetch_assoc();
+?>
 
-        if (password_verify($_POST["Password"], $row["Password"])) {
-            print "First name:" . $row ["First_name"] . "<br>";
-            print "Second name:" . $row ["Second_name"] . "<br>";
-            print "Age:" . $row ["Age"] . "<br>";
-            print "Username:" . $row ["Username"] . "<br>";
-            $countrySelect = $connect ->prepare("SELECT COUONTRY_NAME FROM countries WHERE");
-            $countrySelect->bind_param("i", $row["Nationality"]);
-            $countrySelect->execute();
-            $resultContry= $countrySelect->get_result();
-            $rowCountry = $resultContry->fetch_assoc();
-            print "Country: " . $rowCountry["COUNTRY_NAME"] . "<br>";
-            $_SESSION["UserLogger"] = true;
-        } else {
-            print "Wrong password !";
-        }
-    } else {
-        print "Your username is not in our database !! Please consider registering !"; ?> <a href="Signup.php">Go to the signup page</a>
-            <a href="Login.php">Try again</a>
-        <?php
-    }
-} else {
-     ?>
-<form action="Login.php" method="post">
-    Username: <input type="text" name="Username" required><br>
-    Password: <input type="text" name="Password" required><br>    
-    <input type="submit" name="Login" value="Login">
+  <form action="login_page.php" method="post">
+  <input type="submit" name="Logout" value="Logout">
 </form>
 <?php
-}
-?>
+if (isset($_POST["Logout"])) {
+
+  session_unset();
+  session_destroy();
+  print "You have been successfully logged-out";
+  ?>
+<a href="login_page.php"> Click here to loggin again </a>
+<?php
+} elseif ($_SESSION["UserLogged"]) {
+  print "You have already been logge-in" . "<br>";
+  displayUserDetails($connection);
+} elseif (isset($_POST["Username"]) && isset($_POST["Password"])) {
+  //include_once "credentials.php"; // Create connection
+  // Check connection
+  $userFromMyDatabase = $connection->prepare(
+    "SELECT * FROM ppl WHERE UserName=?"
+  );
+  $userFromMyDatabase->bind_param("s", $_POST["Username"]);
+  $userFromMyDatabase->execute();
+  $result = $userFromMyDatabase->get_result();
+  if ($result->num_rows === 1) {
+    print "Your password is being verified <BR>";
+    $row = $result->fetch_assoc();
+    if (password_verify($_POST["Password"], $row["Password"])) {
+      $_SESSION["UserLogged"] = true;
+      $_SESSION["CurrentUser"] = $row["PERSON_ID"];
+      displayUserDetails($connection);
+    } else {
+      print "Wrong password ! Please type your password correctly";
+    }
+  } else {
+    print "The username you typed has not been found in our database !! Please register first !"; ?> <a href="Signup.php">Click here to register</a> <br>
+        <a href="login_page.php">Try again loggin-in</a>
+    <?php
+  }
+} else {
+   ?>
+    <form action="login_page.php" method="post">
+        Username: <input type="text" name="Username" placeholder="Your name.." required><br>
+        Password: <input type="password" name="Password" placeholder="Password" required><br>
+        <input type="submit" name="Login" value="Login">
+    </form>
+<?php
+} ?>
--- sessionCheck.php
+++ sessionCheck.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-//we need a boolian verable that thells us if the usder has already logged in or not
 if (!issest($_SESSION["UserLogger"])){
     $_SESSION["UserLogger"] = false;    
 }
