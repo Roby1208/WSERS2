338455c6e80d48b0f8184bcda15a3654e40b4d96
roby <triro299@school.lu>
2020-04-27 15:41:09 +0200

    admin

--- administration.php
+++ administration.php
@@ -3,8 +3,15 @@
 include_once "sessionCheck.php";
 include_once "credentials.php";
 
-if($_SESSION["UserLogged"])
+if(!$_SESSION["UserLogged"])
 {
     die("You are not logged in an not an administrator!");
 }
- 
+$UserSelect = $connection ->prepare("SELECT UsrType FROM PPL WHERE PERSON_ID=?");
+$UserSelect->bind_param("i", $_SESSION["CurrentUser"]);
+$UserSelect->execute();
+$resultUser= $UserSelect->get_result();
+$rowUser = $resultUser->fetch_assoc();
+if($rowUser["UsrType"]!== 1){
+die ("You are not an admin");
+}
